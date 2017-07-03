29b1f79879188001b05e97d2faaf56bb851440b0
ronofri11 <renato.onofri@foris.cl>
2017-07-02 23:42:56 -0400

    se cambian colores de icono en las tarjetas para que cuadre con progress bar

--- client/app/indicadores/tarjeta.component.js
+++ client/app/indicadores/tarjeta.component.js
@@ -8,16 +8,21 @@
       var self = this;
       
       self.$onInit = function(){
-      	if(self.config.percent > 50){
-      		self.pass = "check";
-      		self.passColor = "#77B346";
-          self.passClass = "fa-check";
-        }
-        else{
+      	if(self.config.percent <= 33){
           self.pass = "close";
           self.passColor = "#EF5350";
           self.passClass = "fa-times";
-      	}
+        }
+        else if(self.config.percent <= 66){
+          self.pass = "check";
+          self.passColor = "#E2B512";
+          self.passClass = "fa-times";
+        }
+        else {
+          self.pass = "check";
+          self.passColor = "#77B346";
+          self.passClass = "fa-check";
+        }
       };
     }
   });
